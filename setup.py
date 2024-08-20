d920311a65335c95c1cb26fd8a91a12898006664
Tsche <Tsche@users.noreply.github.com>
2024-08-20 03:18:30 +0200

    finalize

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 from setuptools import setup, find_packages
 from setuptools.command.build_py import build_py
 
@@ -8,17 +8,21 @@ class BuildPy(build_py):
         super().run()
 
         name = "magic_codec.pth"
+        input_file = Path("src") / name
+        output_file = Path(self.build_lib) / name
+
         # install path configuration file
-        self.copy_file(name, os.path.join(self.build_lib, name), preserve_mode=0)
+        self.copy_file(str(input_file), str(output_file), preserve_mode=0)
+
 
 setup(
-   name="magic_codec",
-   cmdclass={"build_py": BuildPy},
-   package_dir={'': 'src'},
-   packages=find_packages(
-        where='src',
-    ),
-   version="0.0.1"
+    name="magic_codec",
+    version="0.0.1",
+    description="Extensible codec that can be used to drastically change the behavior of the Python interpreter.",
+    author="Tsche",
+    author_email="contact@palliate.io",
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    include_package_data=True,
+    cmdclass={"build_py": BuildPy}
 )
-
-print("SETUP")
